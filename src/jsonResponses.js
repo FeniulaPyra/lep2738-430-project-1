2496a912a81336372905f4ae3ba9cee963686a14
Lauren Puglise <feniuldragon@gmail.com>
2021-03-03 18:50:08 -0500

    fixed duplicate recipe

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -16,108 +16,80 @@ const recipes = {
       },
     ],
     steps: [
-      {text:'Put a pan on the stove and cover it with butter.'},
-      {text:'turn the stovetop on'},
-      {text:'crack the 1 egg into the pan'},
-      {text:'wait about a minute, then flip the egg over'},
-      {text:'wait another minute, then turn the stove off'},
-      {text:'your egg is done!'},
-      {text:'put it on a plate and eat it, or eat it straight out of the pan!'},
+      { text: 'Put a pan on the stove and cover it with butter.' },
+      { text: 'turn the stovetop on' },
+      { text: 'crack the 1 egg into the pan' },
+      { text: 'wait about a minute, then flip the egg over' },
+      { text: 'wait another minute, then turn the stove off' },
+      { text: 'your egg is done!' },
+      { text: 'put it on a plate and eat it, or eat it straight out of the pan!' },
     ],
   },
-  'kraft mac and cheese bowl':{
-    "name": "Kraft Mac and Cheese bowl",
-    "ingredients": [
+  'kraft mac and cheese bowl': {
+    name: 'Kraft Mac and Cheese bowl',
+    ingredients: [
       {
-        "name": "Kraft mac and cheese bowl",
-        "amount": "1",
-        "unit": "",
-        link: "https://www.kraftmacandcheese.com/products/100166000003/microwavable"
-      }
+        name: 'Kraft mac and cheese bowl',
+        amount: '1',
+        unit: '',
+        link: 'https://www.kraftmacandcheese.com/products/100166000003/microwavable',
+      },
     ],
-    "steps": [
-      {"text": "remove the plastic cover on the bowl"},
-      {"text": "pour tap water into the bowl up to the indicated \"fill line\""},
-      {"text": "stir the macaroni in the water"},
-      {"text": "microwave the macaroni for 3:30 minutes"},
-      {"text": "take the bowl of mac out of the microwave, and stir in the provided cheese dust"},
-      {"text": "enjoy!"}
-    ]
-  },
-  'campbell\'s soup can':{
-    "name": "Campbell's Soup Can",
-    "ingredients": [
-      {
-        "name": "Campbell's Soup can",
-        "amount": "1",
-        "unit": "",
-        "link": ""
-      },
-      {
-        "name": "bowl",
-        "amount": "1",
-        "unit": "",
-        "link": ""
-      }
+    steps: [
+      { text: 'remove the plastic cover on the bowl' },
+      { text: 'pour tap water into the bowl up to the indicated "fill line"' },
+      { text: 'stir the macaroni in the water' },
+      { text: 'microwave the macaroni for 3:30 minutes' },
+      { text: 'take the bowl of mac out of the microwave, and stir in the provided cheese dust' },
+      { text: 'enjoy!' },
     ],
-    "steps": [
-      {
-        "text": "Open the can of soup and pour it into the bowl"
-      },
-      {
-        "text": "Heat the bowl of soup in the microwave for about 2:30 minutes (for 1100 watt microwave)"
-      },
-      {
-        "text": "take the bowl out once it's done, and enjoy!"
-      }
-    ]
   },
   'campbell\'s soup can': {
-    "name": "Campbell's Soup Can",
-    "ingredients": [
+    name: "Campbell's Soup Can",
+    ingredients: [
       {
-        "name": "Campbell's Soup",
-        "amount": "1",
-        "unit": "can",
-        "link": ""
+        name: "Campbell's Soup",
+        amount: '1',
+        unit: 'can',
+        link: '',
       },
       {
-        "name": "bowl",
-        "amount": "1",
-        "unit": "",
-        "link": ""
-      }
+        name: 'bowl',
+        amount: '1',
+        unit: '',
+        link: '',
+      },
     ],
-    "steps": [
-      {"text": "Open the can of soup and pour it into the bowl"},
-      {"text": "Heat the bowl of soup in the microwave for about 2:30 minutes (for 1100 watt microwave)"},
-      {"text": "take the bowl out once it's done, and enjoy!"}
-    ]
+    steps: [
+      { text: 'Open the can of soup and pour it into the bowl' },
+      { text: 'Heat the bowl of soup in the microwave for about 2:30 minutes (for 1100 watt microwave)' },
+      { text: "take the bowl out once it's done, and enjoy!" },
+    ],
   },
-  'boil pasta':{
-    "name": "Boil Pasta",
-    "ingredients": [
+  'boil pasta': {
+    name: 'Boil Pasta',
+    ingredients: [
       {
-        "name": "pasta",
-        "amount": "1",
-        "unit": "box",
-        "link": ""
+        name: 'pasta',
+        amount: '1',
+        unit: 'box',
+        link: '',
       },
       {
-        "name": "sauce",
-        "amount": "1",
-        "unit": "jar",
-        "link": ""
-      }
+        name: 'sauce',
+        amount: '1',
+        unit: 'jar',
+        link: '',
+      },
     ],
-    "steps": [
-      {"text": "Boil 3 quarts of water on the stove"},
-      {"text": "once the water is boiling, pour the pasta in"},
-      {"text": "boil the pasta for 6-8 minutes, stirring occasionally"},
-      {"text": "drain the pasta"},
-      {"text": "serve the pasta with your favorite sauce."}
-    ]
-  }
+    steps: [
+      { text: 'Boil 3 quarts of water on the stove' },
+      { text: 'once the water is boiling, pour the pasta in' },
+      { text: 'boil the pasta for 6-8 minutes, stirring occasionally' },
+      { text: 'drain the pasta' },
+      { text: 'serve the pasta with your favorite sauce.' },
+    ],
+  },
 };
 /*
 let recipe = {
@@ -137,7 +109,7 @@ let recipe = {
     "wait a few minutes",
     "tea is ready!"
   ]
-};*/
+}; */
 const respondJSON = (request, response, statusCode, object) => {
   response.writeHead(statusCode, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(object));
@@ -304,8 +276,8 @@ const handlePutAndPost = (request, response, method, funct) => {
   });
 
   request.on('end', () => {
-    const bodyString = Buffer.concat(body).toString(); 
-    const bodyParams = JSON.parse(bodyString); 
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = JSON.parse(bodyString);
     funct(request, response, bodyParams, method);
   });
 };
